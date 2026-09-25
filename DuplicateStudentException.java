b48da45cad077324c1d8ab32ce42a65c3888f076
akilimali <akiliarsenal@gmail.com>
2026-09-25 10:58:59 +0200

    Adding the database and the souurce code

--- DuplicateStudentException.java
+++
@@ -1,9 +0,0 @@
-/**
- * Custom checked exception thrown when attempting to add a student
- * with an ID that already exists.
- */
-public class DuplicateStudentException extends Exception {
-    public DuplicateStudentException(String message) {
-        super(message);
-    }
-}
--- StudentNotFoundException.java
+++
@@ -1,8 +0,0 @@
-/**
- * Custom checked exception thrown when a student record cannot be found.
- */
-public class StudentNotFoundException extends Exception {
-    public StudentNotFoundException(String message) {
-        super(message);
-    }
-}
